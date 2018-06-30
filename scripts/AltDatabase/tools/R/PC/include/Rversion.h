ad3cc6eccd61bd6685ce58998d8013662709eef0
venkatmi <venkatmi@mail.uc.edu>
2018-06-30 00:50:00 -0400

    1.1.4 beta 6/29/17

---
+++ scripts/AltDatabase/tools/R/PC/include/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 134913
+#define R_NICK "Roasted Marshmallows"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "2"
+#define R_MINOR  "15.1"
+#define R_STATUS ""
+#define R_YEAR   "2012"
+#define R_MONTH  "06"
+#define R_DAY    "22"
+#define R_SVN_REVISION "59607"
+#define R_FILEVERSION    2,151,59607,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
